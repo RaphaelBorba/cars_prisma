f1fe6878fa35993b720e8ff92c4bc2818ba8bfd3
Raphael Borba <raphaelbsx@gmail.com>
2023-01-26 16:02:51 -0300

    refact: delete route done

--- src/repository/carRepository.ts
+++ src/repository/carRepository.ts
@@ -23,7 +23,9 @@ async function createCar(model: string, licensePlate: string, year: string, colo
 }
 
 async function deleteCar(id: number) {
-  await db.query(`DELETE FROM cars WHERE id = $1`, [id]);
+  await prisma.cars.delete({
+    where:{id}
+  })
 }
 
 const carRepository = {
